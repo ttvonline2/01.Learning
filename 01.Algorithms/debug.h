b3fec08a79be4d00823893bcd667fa8f1b640fd1
vang.truong <vang.truong@lge.com>
2025-03-04 11:46:34 +0700

    modify debug

--- 01.Algorithms/debug.h
+++ 01.Algorithms/debug.h
@@ -1,90 +1,14 @@
 #pragma once
-#ifndef _GLIBCXX_NO_ASSERT
-#include <cassert>
-#endif
-#include <cctype>
-#include <cerrno>
-#include <cfloat>
-#include <ciso646>
-#include <climits>
-#include <clocale>
-#include <cmath>
-#include <csetjmp>
-#include <csignal>
-#include <cstdarg>
-#include <cstddef>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-
-#include <ccomplex>
-#include <cfenv>
-#include <cinttypes>
-#include <cstdalign>
-#include <cstdbool>
-#include <cstdint>
-#include <ctgmath>
-#include <cwchar>
-#include <cwctype>
-
-// C++
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
-#include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
-#include <vector>
-
-#include <array>
-#include <atomic>
-#include <chrono>
-#include <condition_variable>
-#include <forward_list>
-#include <future>
-#include <initializer_list>
-#include <mutex>
-#include <random>
-#include <ratio>
-#include <regex>
-#include <scoped_allocator>
-#include <system_error>
-#include <thread>
-#include <tuple>
-#include <typeindex>
-#include <type_traits>
-#include <unordered_map>
-#include <unordered_set>
+#include "vstd.h"
 using namespace std;
 using namespace std::chrono;
+
 //*** debug(x) ***//
+#if OFF_DEBUG
+#define debug(x) //*** debug ***//
+#else
 #define debug(x) cout << "[" << #x << "]" << " : " << (x) << endl
+#endif
 
 #define vangtruong  freopen("D:/01.Learning/00.Ultility/input.txt", "r", stdin); freopen("D:/01.Learning/00.Ultility/output.txt", "w", stdout); auto start_time = high_resolution_clock::now() ; thread timeThread(timer_limitation); timeThread.detach()
 #define hihihaha auto end_time = high_resolution_clock::now();  auto duration_time = duration_cast<milliseconds>(end_time - start_time); cout << "Running time: " << duration_time.count() << " ms\n" ; finished_program = true
@@ -102,60 +26,59 @@ ostream & operator << (ostream & os, pair <X, Y> const& p) {
 
 bool finished_program = false;
 
+
+#if OFF_DEBUG
+#define DB(...) //*** debug ***//
+#else
+// Vector<vector>>
 template <typename T>
-void debugv(const vector<vector<T>>& _arr) {
+void DB(const vector<vector<T>>& _arr, string name = "", string SPACE = " ") {
     int _maxSize = 0;
     for (const auto& x : _arr) _maxSize = max(_maxSize, static_cast<int>(x.size()));
 
-    cout << "Array size [R][C]: [" << _arr.size() << "][" << (_arr.empty() ? 0 : _arr[0].size()) << "]\n";
+    cout << name  << " :Size [R][C]: [" << _arr.size() << "][" << (_arr.empty() ? 0 : _arr[0].size()) << "]\n";
 
     // Print column indices
-    cout << "   [R]   \t";
-    for (int i = 0; i < _maxSize; i++) cout << i << "   \t";
+    cout << SPACE << "[R]" << SPACE << "\t";
+    for (int i = 0; i < _maxSize; i++) cout << i << SPACE << "\t";
 
     for (size_t r = 0; r < _arr.size(); r++) {
-        cout << "\n   [" << r << "]   \t";
+        cout << "\n" << SPACE << "[" << r << "]" << SPACE << "\t";
         for (size_t c = 0; c < _arr[r].size(); c++) {
-            cout << static_cast<int>(_arr[r][c]) << "   \t"; // Ép kiểu về int
+            cout << static_cast<int>(_arr[r][c]) << SPACE << "\t"; // Ép kiểu về int
         }
     }
-    cout << "\n--- END Array debug ---\n";
+    cout << "\n--- END Vector<Vector> debug ---\n";
 }
 
+// Vector 
 template <typename T>
-void debugv(const vector<T>& arr) {
+void DB(const vector<T>& arr, string name = "", string SPACE = " ") {
     // Print index header
-    cout << "[" << arr.size() << "] i:  \t";
-    for (size_t i = 0; i < arr.size(); i++) {
-        cout << i << "    \t";
-    }
+    cout << name  << " Size: [" << arr.size() << "]\n";
+    // Print column indices
+    cout <<  "[Index]" << SPACE << "\t";
+    for (int i = 0; i < arr.size(); i++) cout << i << SPACE << "\t";
     cout << "\n";
 
     // Print values
-    cout << "Value:  \t";
+    cout <<  "[Value]" << SPACE << "\t";
     for (const auto& val : arr) {
-        cout << val << "    \t";
+        cout << val << SPACE << "\t";
     }
-    cout << "\n--- END Array debug ---\n";
+    cout << "\n--- END Vector debug ---\n";
 }
 
-// Template specialization cho vector<bool>
-template <>
-void debugv(const vector<bool>& arr) {
-    cout << "Array size: " << arr.size() << "\n";
 
-    cout << "Index:   ";
-    for (size_t i = 0; i < arr.size(); i++) {
-        cout << i << "   ";
-    }
-    cout << "\n";
-
-    cout << "Values:  ";
-    for (size_t i = 0; i < arr.size(); i++) {
-        cout << (arr[i] ? "1" : "0") << "   ";  // Chuyển bool thành số
-    }
-    cout << "\n--- END Array debug ---\n";
+// mutiple args
+void DB(string name, auto... args) {
+    cout << name << ": ";
+    ((std::cout << args << ","), ...);
+    std::cout << '\n';
 }
+
+#endif
+
 
 
 void timer_limitation() {
