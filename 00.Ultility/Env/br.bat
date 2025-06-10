2baaa33f8b93954dc8a5cfe1e06b47383c8c3a05
vang.truong <vang.truong@lge.com>
2025-06-10 13:54:02 +0700

    add

---
+++ 00.Ultility/Env/br.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+REM Kiểm tra đối số đầu vào
+if "%~1"=="" (
+    echo Usage: br filename
+    exit /b 1
+)
+
+REM Lấy tên file không có phần mở rộng
+set "filename=%~n1"
+set "ext=%~x1"
+
+REM Kiểm tra xem phần mở rộng có phải là .cpp không
+if /I not "%ext%"==".cpp" (
+    echo Error: Only .cpp files are supported.
+    exit /b 1
+)
+
+REM Biên dịch bằng g++
+g++ "%~1" -o "%filename%.exe"
+if errorlevel 1 (
+    echo Compilation failed.
+    exit /b 1
+)
+
+REM Chạy chương trình
+echo Running %filename%.exe ...
+"%filename%.exe"
+
+endlocal
